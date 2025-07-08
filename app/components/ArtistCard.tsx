7a551dd3cf6528843559cce8de5cccc5daa1ee57
elinaols <elol0031@student.hv.se>
2025-07-08 17:17:05 +0200

    Adding styling and functionality

--- app/components/ArtistCard.tsx
+++ app/components/ArtistCard.tsx
@@ -10,15 +10,14 @@ interface ArtistProps {
 }
 */
 export default function ArtistCard() {
-
     return (
-        <div className={`flex flex-col self-center border border-(--accent) hover:border-(--secondary) p-4 text-start rounded-2xl relative w-[140px] h-[450px] flex-1 hover:flex-[3] transition-all duration-300`}>
+        <div className={`flex flex-col self-center border border-(--accent) hover:border-(--secondary) cursor-grabbing p-4 text-start rounded-2xl relative w-[140px] h-[450px] flex-1 hover:flex-[3] transition-all duration-300`}>
             <div className='z-10'>
                 <p>Kendrick Lamar</p>
                 <p>heart pt.6</p>
                 <p>GNX</p>
             </div>
-            <Image src="/rapper.jpg" fill className='object-cover rounded-2xl' sizes='200px' alt="Rapper on stage" />
+            <Image src="/rapper.jpg" fill className='object-cover rounded-2xl' sizes='300px' alt="Rapper on stage" />
         </div>
     )
 }
--- app/components/SearchOnSpotify.tsx
+++ app/components/SearchOnSpotify.tsx
@@ -1,15 +1,18 @@
 'use client'
-import React, {useState} from 'react'
+import React from 'react'
+import { useSession } from 'next-auth/react'
 
 export default function SearchOnSpotify() {
-    const [disable, setDisable] = useState<boolean>(false)
+    const { data: session } = useSession()
+
+    const disable = session ? 'cursor-grab' : 'cursor-not-allowed'
     
         return (
         <div className='flex justify-center gap-4 pb-4 pt-10'> 
-            <input type="text" className='border border-(--accent) rounded-2xl p-6 min-w-[500px]'/>
+            <input type="text" className='border border-(--secondary) hover:border-(--accent) rounded-2xl p-6 min-w-[500px]'/>
             <button 
-                onClick={() => setDisable(prev => !prev)} 
-                className={`${disable ? 'cursor-not-allowed' : 'cursor-grab'} font-bold hover:bg-(--accent) bg-(--secondary) hover:scale-102 rounded-2xl p-4`}
+                className={`${disable} font-bold hover:bg-(--accent) bg-(--secondary) hover:scale-102 rounded-2xl p-6`}
+                disabled={!session}
                 >
                     Sök
             </button>
