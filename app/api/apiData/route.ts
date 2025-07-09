4796526730401215598ddaa7bf8637407fc7bd62
elinaols <elol0031@student.hv.se>
2025-07-09 13:58:48 +0200

    Correct API route to use named exports for HTTP methods in app router

--- app/api/apiData/route.ts
+++ app/api/apiData/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../auth/[...nextauth]/route'
 
-export default async function GET(req: NextRequest) {
+export async function GET(req: NextRequest) {
     const {searchParams} = new URL(req.url)
     const query = searchParams.get('query') ?? ''
 
