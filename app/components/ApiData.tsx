333ca85a13e310288a971bfb8d5e5b13e837db9c
elinaols <elol0031@student.hv.se>
2025-07-09 12:04:11 +0200

    Fix: Encoded query

--- app/components/ApiData.tsx
+++ app/components/ApiData.tsx
@@ -1,19 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
-import React from 'react'
 
 export default async function ApiData(
     req: NextApiRequest, 
     res: NextApiResponse
 ) {
     const query = req.query?.query
+
+    if (!query || Array.isArray(query)) return res.status(400).json({ error: 'Invalid or missing query parameter' })
+
     const session = await getSession({req})
 
     if (!session?.token?.accessToken) {
         return res.status(401).json({error: "No authenticated user"})
     }
 
-    const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&market=from_token&type=album,track,artist,playlist&limit=5`,
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&market=from_token&type=album,track,artist,playlist&limit=5`,
         {
             headers: {
                 Authorization: `Bearer ${session.token.accessToken}`,
@@ -25,13 +27,6 @@ export default async function ApiData(
     if (!response.ok) return res.status(response.status).json({error: 'Spotify API error'})
 
     const results = await response.json()
-    console.log(results)
-
-    return (
-        <>
-            {/*posts.map((post: Post) => (
-                <li className='list-none' key={post.id}>{post.name}</li>
-            ))*/}
-        </>
-    )
+    
+    return res.status(response.status).json(results)
 }
