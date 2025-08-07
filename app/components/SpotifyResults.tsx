469c4db53c26e31860dc056314c7d9acd611ac66
elinaols <elol0031@student.hv.se>
2025-08-07 15:25:53 +0200

    Small fix in the ui

--- app/components/SpotifyResults.tsx
+++ app/components/SpotifyResults.tsx
@@ -60,8 +60,9 @@ export default function SpotifyResults({results, type}: Props) {
 		console.log(item, index)
 		return (
 			<div key={index} className="py-10 px-50 flex w-full justify-center gap-8">
-				{/** Checks every item in the ui */}
-				{JSON.stringify(item)}
+				{/** Checks every item in the ui 
+                    JSON.stringify(item)*/
+                }
                 {type === 'artist' &&
                     <ArtistCard
                         name={item.name}
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    // Allow external image from Spotifys domains (Enables loading optimized images)
     domains: ['i.scdn.co', 'mosaic.scdn.co']
   }
 };
