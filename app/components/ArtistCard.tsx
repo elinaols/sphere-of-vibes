e57841b9862db68f3a90877de2883b87ce43619f
elinaols <elol0031@student.hv.se>
2025-08-07 15:27:56 +0200

    Small fix

--- app/components/ArtistCard.tsx
+++ app/components/ArtistCard.tsx
@@ -18,7 +18,7 @@ export default function ArtistCard({name, imageUrl, followers, genre, date, albu
         <div className={`flex flex-col self-center border border-(--accent) hover:border-(--secondary) cursor-grabbing p-4 text-start rounded-2xl relative w-[140px] h-[450px] flex-1 hover:flex-[3] transition-all duration-300`}>
             <div className='z-10'>
                 <p>{name}</p>
-                <p>{`$followers listeners`}</p>
+                <p>{`${followers} listeners`}</p>
                 <p>{genre}</p>
                 <p>{date}</p>
                 <p>{album}</p>
