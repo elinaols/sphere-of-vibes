deac22e60bebc79b4d304aceaf462eb242da69c5
elinaols <elol0031@student.hv.se>
2025-08-18 14:24:28 +0200

    Small fix for the listeners

--- app/components/SpotifyResults.tsx
+++ app/components/SpotifyResults.tsx
@@ -67,7 +67,7 @@ export default function SpotifyResults({results, type}: Props) {
 				}
 
 				// Adding extra props depending on the item type
-				if ("artist" in item && "followers" in item) {
+				if ("followers" in item && type === 'artist') {
 					// Artist
 					props.followers = item.followers.total
 					props.genre = "genres" in item ? item.genres[0] : "Unknown genre"
