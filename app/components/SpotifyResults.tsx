77817cc3390563a045b4e2a890ff5cd03a8bd744
elinaols <elol0031@student.hv.se>
2025-08-18 15:34:15 +0200

    test

--- app/components/SpotifyResults.tsx
+++ app/components/SpotifyResults.tsx
@@ -75,9 +75,7 @@ export default function SpotifyResults({results, type}: Props) {
 				} else if ("album" in item) {
 					// Album/Track
 					props.album = item.album.name
-					props.date = type === "track"
-						? props.date = item.album.release_date
-						: props.date = item.release_date
+					props.date = item.release_date
 					props.artistName = item.artists.name
 				} else if ("owner" in item) {
 					// Playlist
