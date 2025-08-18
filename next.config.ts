b7163cbdd503262c60a3b924992b182b5aeead83
elinaols <elol0031@student.hv.se>
2025-08-18 11:42:00 +0200

    Fix: Added spotify cdn

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     // Allow external image from Spotifys domains (Enables loading optimized images)
-    domains: ['i.scdn.co', 'mosaic.scdn.co', 'image-cdn-ak.spotifycdn.com', 'image-cdn-ck-spotifycdn.com', 'image-cdn-bk-spotifycdn.com']
+    domains: ['i.scdn.co', 'mosaic.scdn.co', 'image-cdn-ak.spotifycdn.com', 'image-cdn-ck-spotifycdn.com', 'image-cdn-bk-spotifycdn.com', 'image-cdn-fa-spotifycdn.com']
   }
 };
 
