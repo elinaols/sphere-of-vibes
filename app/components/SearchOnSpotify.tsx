26f41dc90f2eeb9c9404220f3f266b50ae2faf9d
elinaols <elol0031@student.hv.se>
2025-08-23 19:50:54 +0200

    add white text color for input

--- app/components/SearchOnSpotify.tsx
+++ app/components/SearchOnSpotify.tsx
@@ -1,81 +1,87 @@
 "use client"
-import React, { FormEvent, useState } from "react"
+import React, {FormEvent, useState} from "react"
 import {useSession} from "next-auth/react"
-import { SpotifyResultsData, SpotifySearchType } from "@/types/json"
+import {SpotifyResultsData, SpotifySearchType} from "@/types/json"
 import PopUp from "./PopUp"
 
 // TODO: Code comments is missing
 type Props = {
-    setResults: (results: SpotifyResultsData) => void
-    setType: (type: SpotifySearchType) => void
+	setResults: (results: SpotifyResultsData) => void
+	setType: (type: SpotifySearchType) => void
 }
 
 export default function SearchOnSpotify({setResults, setType}: Props) {
-	
-    const {data: session} = useSession()
+	const {data: session} = useSession()
 
 	const disable = session ? "cursor-grab" : "cursor-not-allowed"
 
-    const [popUpMessage, setPopUpMessage] = useState<string | null>(null)
+	const [popUpMessage, setPopUpMessage] = useState<string | null>(null)
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if(!session) {
-            e.preventDefault()
-            setPopUpMessage("Logga in först!")
-        }
-    }
+	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		if (!session) {
+			e.preventDefault()
+			setPopUpMessage("Logga in först!")
+		}
+	}
 
-    async function onSubmit(event: FormEvent<HTMLFormElement>) {
-        try {
-            event.preventDefault()
-    
-            const formData = new FormData(event.currentTarget)
-            const query = formData.get('query')
-            const type = formData.get('type')
+	async function onSubmit(event: FormEvent<HTMLFormElement>) {
+		try {
+			event.preventDefault()
 
-            if (typeof type === 'string') setType(type as SpotifySearchType)
+			const formData = new FormData(event.currentTarget)
+			const query = formData.get("query")
+			const type = formData.get("type")
 
-            if (!query) {
-                setPopUpMessage('Fyll i sökfältet')
-                return
-            } 
-    
-            const response = await fetch(`/api/apiData?query=${encodeURIComponent(query as string)}&type=${encodeURIComponent(type as string)}`)
-    
-            if (!response.ok) throw new Error('Failed to fetch data.')
-    
-            const data = await response.json()
-            setResults(data)
-        } catch (error) {
-            console.error("Error while fetching data", error)
-        } 
-    }
+			if (typeof type === "string") setType(type as SpotifySearchType)
+
+			if (!query) {
+				setPopUpMessage("Fyll i sökfältet")
+				return
+			}
+
+			const response = await fetch(
+				`/api/apiData?query=${encodeURIComponent(query as string)}&type=${encodeURIComponent(type as string)}`
+			)
+
+			if (!response.ok) throw new Error("Failed to fetch data.")
+
+			const data = await response.json()
+			setResults(data)
+		} catch (error) {
+			console.error("Error while fetching data", error)
+		}
+	}
 
 	return (
-        <>
-            <form onSubmit={onSubmit} className="text-(--text-color) flex flex-col sm:flex-row justify-center gap-4 pb-4 pt-10 px-2 sm:px-0 w-[90%] self-center">
-                <input
-                    name="query"
-                    type="text"
-                    aria-label="Sök"
-                    className="border border-(--secondary) hover:border-(--accent) rounded-2xl w-full sm:w-[50%] p-3 md:p-4 lg:p-6"
-                />
-                <select aria-label="Välj typ av sökning" name="type" defaultValue="artist" className={`${disable} font-bold hover:bg-(--accent) text-center sm:text-start bg-(--secondary) rounded-2xl p-3 md:p-4 lg:p-6 appearance-none bg-[url(/arrow-down.png)] bg-no-repeat bg-[right_0.3rem_center]`} >
-                    <option value="artist">Artist</option>
-                    <option value="track">Låt</option>
-                    <option value="album">Album</option>
-                    <option value="playlist">Spellista</option>
-                </select>
-                <button
-                    type="submit"
-                    className={`${disable} cursor-grab font-bold hover:bg-(--accent) bg-(--secondary) hover:scale-102 rounded-2xl p-3 md:p-4 lg:p-6`}
-                    onClick={handleClick}
-                >
-                    Sök
-                </button>
-            </form>
-            {/* popUpMessage will not render when it's null */}
-            {popUpMessage && <PopUp message={popUpMessage} onClose={() => setPopUpMessage(null)}/>}
-        </>
+		<>
+			<form
+				onSubmit={onSubmit}
+				className="flex flex-col sm:flex-row justify-center gap-4 pb-4 pt-10 px-2 sm:px-0 w-[90%] self-center">
+				<input
+					name="query"
+					type="text"
+					aria-label="Sök"
+					className="text-(--text-color) border border-(--secondary) hover:border-(--accent) rounded-2xl w-full sm:w-[50%] p-3 md:p-4 lg:p-6"
+				/>
+				<select
+					aria-label="Välj typ av sökning"
+					name="type"
+					defaultValue="artist"
+					className={`${disable} font-bold hover:bg-(--accent) text-center sm:text-start bg-(--secondary) rounded-2xl p-3 md:p-4 lg:p-6 appearance-none bg-[url(/arrow-down.png)] bg-no-repeat bg-[right_0.3rem_center]`}>
+					<option value="artist">Artist</option>
+					<option value="track">Låt</option>
+					<option value="album">Album</option>
+					<option value="playlist">Spellista</option>
+				</select>
+				<button
+					type="submit"
+					className={`${disable} cursor-grab font-bold hover:bg-(--accent) bg-(--secondary) hover:scale-102 rounded-2xl p-3 md:p-4 lg:p-6`}
+					onClick={handleClick}>
+					Sök
+				</button>
+			</form>
+			{/* popUpMessage will not render when it's null */}
+			{popUpMessage && <PopUp message={popUpMessage} onClose={() => setPopUpMessage(null)} />}
+		</>
 	)
 }
