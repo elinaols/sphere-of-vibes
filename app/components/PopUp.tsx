21c469bbdad4e3f099db9a36133b4186bf63867f
elinaols <elol0031@student.hv.se>
2025-08-23 18:55:17 +0200

    Fix: design for popup

--- app/components/PopUp.tsx
+++ app/components/PopUp.tsx
@@ -7,9 +7,11 @@ type Props = {
 
 export default function PopUp({message, onClose}: Props) {
     return (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-1 py-3 md:p-8 lg:p-14 bg-(--popup-white) flex self-center flex-col rounded items-center z-50">
-            <p className="px-2 pb-2 font-bold text-(--black)">{message}</p>
-            <button onClick={onClose} className="p-[0.3rem] md:p-[0.6rem] text-(--white) bg-(--secondary) hover:bg-(--accent) rounded">Close</button>
+        <div className="fixed inset-0 flex items-center justify-center">
+            <div className="w-[70%] sm:w-1/2 p-6 sm:p-8 lg:p-12 bg-white/90 backdrop-blur-md flex self-center flex-col rounded items-center z-50">
+                <p className="px-2 pb-2 md:pb-4 font-bold text-(--black)">{message}</p>
+                <button onClick={onClose} className="p-[0.3rem] md:p-[0.6rem] text-(--white) bg-(--secondary) hover:bg-(--accent) rounded">Close</button>
+            </div>
         </div>
     )
 }
