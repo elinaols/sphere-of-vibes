879cc0cb6e7bbb8ea864fe6de429f422bf16690d
elinaols <elol0031@student.hv.se>
2025-08-23 17:28:18 +0200

    Fix: responsive design for button and spotify results layout

--- app/components/LoginStatus.tsx
+++ app/components/LoginStatus.tsx
@@ -12,14 +12,14 @@ export default function LoginStatus({darkMode}: Props) {
     if (session) {
         return (
             <div className={`flex flex-col sm:flex-row justify-center p-2 ${darkMode ? 'text-(--accent)' : 'text-(--text-color)'}`}>
-                <p className='pe-3'>Inloggad som <span className='font-bold'>{session.user?.email}</span></p>
+                <p className='pe-3 pb-2 sm:pb-0'>Inloggad som <span className='font-bold'>{session.user?.email}</span></p>
                 <button className='self-center font-bold px-2 border cursor-grab rounded-lg hover:bg-(--accent)' onClick={() => signOut()}>Logga ut</button>
             </div>
         )
     } else {
         return (
             <div className={`flex flex-col sm:flex-row justify-center p-2 ${darkMode ? 'text-(--accent)' : 'text-(--text-color)'}`}>
-                <p className='pe-3'>Inte inloggad</p>
+                <p className='pe-3 pb-2 sm:pb-0'>Inte inloggad</p>
                 <button className='self-center font-bold px-2 border cursor-grab rounded-lg hover:bg-(--accent)' onClick={() => signIn('spotify')}>Logga in med Spotify</button>
             </div>
         )
