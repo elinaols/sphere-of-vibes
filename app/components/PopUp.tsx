5c9ec182762f22dc4b42fca2b3c660344aad3e78
elinaols <elol0031@student.hv.se>
2025-08-23 20:34:53 +0200

    fix: change button text

--- app/components/PopUp.tsx
+++ app/components/PopUp.tsx
@@ -10,7 +10,7 @@ export default function PopUp({message, onClose}: Props) {
         <div className="fixed inset-0 flex items-center justify-center">
             <div className="w-[70%] sm:w-1/2 p-6 sm:p-8 lg:p-12 bg-white/90 backdrop-blur-md flex self-center flex-col rounded items-center z-50">
                 <p className="px-2 pb-2 md:pb-4 font-bold text-(--black)">{message}</p>
-                <button onClick={onClose} className="p-[0.3rem] md:p-[0.6rem] text-(--white) bg-(--secondary) hover:bg-(--accent) rounded">Close</button>
+                <button onClick={onClose} className="p-[0.3rem] md:p-[0.6rem] text-(--white) bg-(--secondary) hover:bg-(--accent) rounded">Stäng</button>
             </div>
         </div>
     )
