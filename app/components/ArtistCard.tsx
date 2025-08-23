fef84b44c831d6b26facd205594e070098222f7c
elinaols <elol0031@student.hv.se>
2025-08-23 17:38:32 +0200

    Small ui fix for button

--- app/components/ArtistCard.tsx
+++ app/components/ArtistCard.tsx
@@ -7,7 +7,7 @@ export default function ArtistCard({name, imageUrl, followers, genre, date, albu
     const capatilize = (str: string): string => str ? str.charAt(0).toUpperCase() + str.slice(1) : ''
     
     return (
-        <div className={`flex flex-col self-center border border-(--accent) hover:border-(--secondary) cursor-grabbing p-4 text-start rounded-2xl relative w-[140px] h-[450px] flex-1 hover:flex-[3] transition-all duration-300`}>
+        <div className={`flex flex-col self-center border border-(--accent) hover:border-(--secondary) cursor-grab p-4 text-start rounded-2xl relative w-[140px] h-[450px] flex-1 hover:flex-[3] transition-all duration-300`}>
             <div className='z-10 bg-[#532ec5]/30 backdrop-invert backdrop-opacity-10 rounded-2xl p-2'>
                 <p>{name}</p>
                 <p>{followers ? `${followers} listeners` : null}</p>
