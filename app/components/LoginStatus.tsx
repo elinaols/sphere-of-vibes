e2788a492621b803417587cfda9a25ac77840202
elinaols <elol0031@student.hv.se>
2025-09-01 12:07:06 +0200

    fix: ensure Spotify session is recognized on initial page load

--- app/components/LoginStatus.tsx
+++ app/components/LoginStatus.tsx
@@ -9,21 +9,25 @@ type Props = {
 export default function LoginStatus({darkMode}: Props) {
     const { data: session, status } = useSession()
     
-    if (status === "loading") return <p>Loading...</p>
+    const isLoading = status === 'loading'
 
-    if (session) {
-        return (
-            <div className={`flex flex-col sm:flex-row justify-center p-2 ${darkMode ? 'text-(--accent)' : 'text-(--text-color)'}`}>
-                <p className='pe-3 pb-2 sm:pb-0'>Inloggad som <span className='font-bold'>{session.user?.email}</span></p>
-                <button className='self-center font-bold px-2 border cursor-grab rounded-lg hover:bg-(--accent)' onClick={() => signOut()}>Logga ut</button>
-            </div>
-        )
-    } else {
-        return (
-            <div className={`flex flex-col sm:flex-row justify-center p-2 ${darkMode ? 'text-(--accent)' : 'text-(--text-color)'}`}>
-                <p className='pe-3 pb-2 sm:pb-0'>Inte inloggad</p>
-                <button className='self-center font-bold px-2 border cursor-grab rounded-lg hover:bg-(--accent)' onClick={() => signIn('spotify')}>Logga in med Spotify</button>
-            </div>
-        )
-    }
+    return (
+        <div className={`flex flex-col sm:flex-row justify-center p-2 ${darkMode ? 'text-(--accent)' : 'text-(--text-color)'}`}>
+            {
+                isLoading ? (
+                    <p>Laddar...</p>
+                ) : session ? (
+                <>
+                    <p className='pe-3 pb-2 sm:pb-0'>Inloggad som <span className='font-bold'>{session.user?.email}</span></p>
+                    <button className='self-center font-bold px-2 border cursor-grab rounded-lg hover:bg-(--accent)' onClick={() => signOut()}>Logga ut</button>
+                </>
+                ) : (
+                <>
+                    <p className='pe-3 pb-2 sm:pb-0'>Inte inloggad</p>
+                    <button className='self-center font-bold px-2 border cursor-grab rounded-lg hover:bg-(--accent)' onClick={() => signIn('spotify')}>Logga in med Spotify</button>
+                </>
+                )
+            }
+        </div>
+    )
 }
