e215cbe086a1971f928bad4ad6f80feb39276b66
elinaols <elol0031@student.hv.se>
2025-09-02 11:08:30 +0200

    todo: create function for refreshing access token

--- app/api/apiData/route.ts
+++ app/api/apiData/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server' // Helps with creating and returning 
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../auth/[...nextauth]/authOptions'
 
+// TODO: Create function for refreshing access token
+
 // Custom request handler to be able to make requests to Spotifys API
 export async function GET(req: Request) {
     // Takes the request URL to get all parameters after ? 
