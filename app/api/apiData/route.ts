d1a569d1a7b049d18cfa542395aaca238011db2f
elinaols <elol0031@student.hv.se>
2025-09-02 10:55:47 +0200

    debug test

--- app/api/apiData/route.ts
+++ app/api/apiData/route.ts
@@ -2,6 +2,48 @@ import { NextResponse } from 'next/server' // Helps with creating and returning 
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../auth/[...nextauth]/authOptions'
 
+interface SpotifyToken {
+    accessToken: string
+    expiresAt: number
+    refreshToken: string
+    error?: string
+}
+
+async function refreshAccessToken(token: SpotifyToken) {
+    try {
+        const url = 'https://accounts.spotify.com/api/token'
+        const body = new URLSearchParams({
+            grant_type: 'refresh_token',
+            refresh_token: token.refreshToken
+        })
+        
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ` + Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64'),
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: body.toString()
+        })
+
+        const refreshedTokens = await response.json()
+        if (!response.ok) throw refreshedTokens
+
+        return {
+            ...token,
+            accessToken: refreshedTokens.access_token,
+            expiresAt: Date.now() + refreshedTokens.expires_in * 1000,
+            refreshToken: refreshedTokens.refresh_token
+        }
+    } catch (error) {
+        console.error('Error refreshing access token:', error)
+        return {
+            ...token,
+            error: 'RefreshAccessTokenFailed'
+        }
+    }
+}
+
 // Custom request handler to be able to make requests to Spotifys API
 export async function GET(req: Request) {
     // Takes the request URL to get all parameters after ? 
